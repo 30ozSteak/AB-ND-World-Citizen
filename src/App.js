b353d4d775f3c53365a5e2faaa2023c3411a4fa5
Nick Dambrosio <ndambro303@gmail.com>
2018-09-05 22:57:58 -0600

    Add login fields

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Welcome from "./Welcome";
+import Login from "./Login";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Welcome />
+        <Login />
       </div>
     );
   }
