5a15e7f3c6f30314aa072d8a61604bbdcfdd25b7
Nick Dambrosio <ndambro303@gmail.com>
2018-09-09 20:21:24 -0600

    new gradient

---
+++ src/Settings.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./icon.css";
+
+export default class Settings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render(props) {
+    return <div className="icon" />;
+  }
+}
