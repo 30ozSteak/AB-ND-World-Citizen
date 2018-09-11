ac9d6e160a521b97a2d9f2ace74a690b08bd138d
Nick Dambrosio <ndambro303@gmail.com>
2018-09-10 18:57:16 -0600

    change icon to butt

--- src/SlickSlider.js
+++ src/SlickSlider.js
@@ -9,7 +9,7 @@ import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
 import Step4 from "./Step4";
-import Icon from "./Icon";
+import Butt from "./Butt";
 
 export default class SlickSlider extends Component {
   render() {
@@ -23,7 +23,7 @@ export default class SlickSlider extends Component {
       <div className="container">
         <Slider {...settings}>
           <div>
-            <Icon />
+            <Butt />
           </div>
           <div>
             <Step1 />
