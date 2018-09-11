ccfa53caceafd1bf16faa44d8540cea067319e28
Nick Dambrosio <ndambro303@gmail.com>
2018-09-10 18:45:23 -0600

    change export method

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,7 +3,6 @@ import React from "react";
 const Welcome = props => {
   return (
     <div className="welcome-message">
-      <div class="spinning-globe" />
       <h1>World Citizen</h1>
       <h5 className>Rewarding You for the Good You Already Do</h5>
     </div>
