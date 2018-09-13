1fb91619ca49d12a7ad19192336de68ea18108bc
Nick Dambrosio <ndambro303@gmail.com>
2018-09-12 22:06:11 -0600

    Info tests

--- src/info-test.js
+++ src/info-test.js
@@ -1,33 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Info from "./Info";
 import { mount, shallow } from "enzyme";
 
-describe("App", () => {
+describe("Info", () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<App />);
+    wrapper = shallow(<Info />);
   });
 
   it("should exist", () => {
     expect(wrapper).toBeDefined();
   });
 
-  it("should render a InfoButton component", () => {
+  it("should render three components without crashing", () => {
     expect(wrapper.find("InfoButton").length).toEqual(1);
+    expect(wrapper.find("MiniGlobe").length).toEqual(1);
+    expect(wrapper.find("NavLink").length).toEqual(1);
   });
 
-  it("should render a Notifications component", () => {
-    expect(wrapper.find("Notifications").length).toEqual(1);
-  });
+  // it("should render a Notifications component", () => {
+  //   expect(wrapper.find("Notifications").length).toEqual(1);
+  // });
 
   // it("should render a PageContent component", () => {
   //   console.log(wrapper.class);
   //   expect(wrapper.find("PageContent").length).toEqual(1);
   // });
 
-  it("should render the Menu component", () => {
-    expect(wrapper.find("Menu").length).toEqual(1);
-  });
+  // it("should render the Menu component", () => {
+  //   expect(wrapper.find("Menu").length).toEqual(1);
+  // });
 });
