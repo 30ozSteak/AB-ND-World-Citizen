49cc7b287e36b64d29b397c4f240f460aa190f8b
Alex Bruce <brucealmightyalex@yahoo.com>
2018-09-13 08:33:18 -0600

    Cleaned up some code

--- src/NearbyCard.js
+++ src/NearbyCard.js
@@ -27,18 +27,16 @@ export default class NearbyCard extends Component {
     return (
       <div className="nearbycard-card" onClick={this.expandCard}>
         <div
-          className={
-            "default-nearbycard " +
-            (!this.state.expanded && "default-card-small")
-          }
-        >
+          className={"default-nearbycard " + (!this.state.expanded && "default-card-small")}>
           <h3 className="nearbycard-business-name">{this.props.name}</h3>
           <p className="nearbycard-distance">{this.props.distance}</p>
         </div>
         <div className="nearbycard-second-row">
           <h3 className="nearbycard-item">{this.props.product}</h3>
           <p className="nearbycard-category">
-            {this.returnCategory(this.props.category)}
+            {
+              this.returnCategory(this.props.category)
+            }
           </p>
         </div>
         <div className={"down-arrow " + (this.state.expanded && "up-arrow")} />
