b6cedd0741174d4c6430f2b768657dc6f04f7768
Nick Dambrosio <ndambro303@gmail.com>
2018-09-20 13:08:33 -0600

    new notification

--- src/Popup.js
+++ src/Popup.js
@@ -13,6 +13,10 @@ export default class Popup extends Component {
       <div className="pop-up-container">
         <div className="pop-up">
           <p> ❗Come check us out at Turing's Demo Night! 9/20</p>
+          <p class="recent-unlock">
+            ❗You've Unlocked: Local Citizenship for your purchase at Warby
+            Parker. Nice job!
+          </p>
         </div>
       </div>
     );
